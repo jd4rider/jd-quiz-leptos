b3b3cee4f79f423d86a4ee4e1c61b070ade928b8
jd4rider <jd4rider@gmail.com>
2023-02-05 04:38:44 -0700

    Trying out a couple more things with vercel

--- server/index.ts
+++ server/index.ts
@@ -113,6 +113,7 @@ const publicDirectoryPath = path.join(__dirname, '../dist')
 
 app.use('/*', express.static(publicDirectoryPath));
 
+
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
