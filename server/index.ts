055cc45adbc86599711d86f0c4efaff4ea946f6d
jd4rider <jd4rider@gmail.com>
2023-02-05 04:52:18 -0700

    another'

--- server/index.ts
+++ server/index.ts
@@ -111,7 +111,7 @@ export const schema = makeExecutableSchema({
 
 const publicDirectoryPath = path.join(__dirname, '../dist')
 
-app.use('/*', express.static(publicDirectoryPath));
+app.use(express.static('../dist'));
 
 
 app.get('/*', function(req, res) {
